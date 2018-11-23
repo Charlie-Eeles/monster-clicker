f9ae5b75de1710195dc67f56a9dba53cdc57db14
charlie <charlie.eeles@hotmail.com>
2018-11-23 23:53:24 +0000

    Updates

---
+++ precache-manifest.ee6cdae10702c8ce30c8afe2b5722f5c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "be4a54065d482936de06588749aeb458",
+    "url": "/monster-clicker/static/media/troll.be4a5406.png"
+  },
+  {
+    "revision": "6a24fc05c6239f654b40649d2b634ed3",
+    "url": "/monster-clicker/static/media/sword.6a24fc05.png"
+  },
+  {
+    "revision": "85962d86cc9601cb7cdc1e2c1f6dbe05",
+    "url": "/monster-clicker/static/media/hammer.85962d86.png"
+  },
+  {
+    "revision": "6cc274ebcfc740c08226be9de6d51f50",
+    "url": "/monster-clicker/static/media/goblin.6cc274eb.png"
+  },
+  {
+    "revision": "88cbb6aa3eab1e5d4aa9",
+    "url": "/monster-clicker/static/js/runtime~main.88cbb6aa.js"
+  },
+  {
+    "revision": "f486e33af11741fadaba",
+    "url": "/monster-clicker/static/js/main.f486e33a.chunk.js"
+  },
+  {
+    "revision": "10781889b59d001711e5",
+    "url": "/monster-clicker/static/js/1.10781889.chunk.js"
+  },
+  {
+    "revision": "f486e33af11741fadaba",
+    "url": "/monster-clicker/static/css/main.1aebf2dc.chunk.css"
+  },
+  {
+    "revision": "8362ded51292cbfb678963f6ce4e470d",
+    "url": "/monster-clicker/index.html"
+  }
+];
