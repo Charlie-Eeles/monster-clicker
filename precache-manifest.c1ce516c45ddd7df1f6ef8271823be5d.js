ed55676e74ae6a845978957f0be5140977c3bed3
charlie <charlie.eeles@hotmail.com>
2018-11-28 18:23:34 +0000

    Updates

---
+++ precache-manifest.c1ce516c45ddd7df1f6ef8271823be5d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0388af62be225877024f72df28975f8",
+    "url": "/monster-clicker/static/media/hillGiant.e0388af6.png"
+  },
+  {
+    "revision": "38a2b6e745f2e4c71582",
+    "url": "/monster-clicker/static/css/main.cd94c1a2.chunk.css"
+  },
+  {
+    "revision": "843104cbe3202a1d045c",
+    "url": "/monster-clicker/static/js/1.843104cb.chunk.js"
+  },
+  {
+    "revision": "88cbb6aa3eab1e5d4aa9",
+    "url": "/monster-clicker/static/js/runtime~main.88cbb6aa.js"
+  },
+  {
+    "revision": "6cc274ebcfc740c08226be9de6d51f50",
+    "url": "/monster-clicker/static/media/goblin.6cc274eb.png"
+  },
+  {
+    "revision": "be4a54065d482936de06588749aeb458",
+    "url": "/monster-clicker/static/media/troll.be4a5406.png"
+  },
+  {
+    "revision": "857ca2f92ab68ab2ca04de460838f9b2",
+    "url": "/monster-clicker/static/media/demon.857ca2f9.png"
+  },
+  {
+    "revision": "a86f3577b27e7927d624a5e7fe425f66",
+    "url": "/monster-clicker/static/media/general.a86f3577.png"
+  },
+  {
+    "revision": "38a2b6e745f2e4c71582",
+    "url": "/monster-clicker/static/js/main.38a2b6e7.chunk.js"
+  },
+  {
+    "revision": "fa605a05127891f5200a9ea62bf3f17c",
+    "url": "/monster-clicker/static/media/iceGiant.fa605a05.png"
+  },
+  {
+    "revision": "b638b1334a6c18b8916e268eee356dfb",
+    "url": "/monster-clicker/static/media/skeleton.b638b133.png"
+  },
+  {
+    "revision": "6a24fc05c6239f654b40649d2b634ed3",
+    "url": "/monster-clicker/static/media/sword.6a24fc05.png"
+  },
+  {
+    "revision": "85962d86cc9601cb7cdc1e2c1f6dbe05",
+    "url": "/monster-clicker/static/media/hammer.85962d86.png"
+  },
+  {
+    "revision": "763c974518b7e10181d895eb22010aff",
+    "url": "/monster-clicker/static/media/scimitar.763c9745.png"
+  },
+  {
+    "revision": "94e5a4c9e065ff6fcbde92648486aea2",
+    "url": "/monster-clicker/static/media/godSword.94e5a4c9.png"
+  },
+  {
+    "revision": "da30e0b2a6391864a07fd39ca3a2362e",
+    "url": "/monster-clicker/index.html"
+  }
+];
