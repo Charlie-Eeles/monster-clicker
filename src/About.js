62790c7c3f3251818ea172252ca16f1ad5984765
charlie <charlie.eeles@hotmail.com>
2018-11-28 18:09:31 +0000

    improved navigation// added "about"

--- src/About.js
+++ src/About.js
@@ -9,12 +9,22 @@ const About = (props) => {
     return(
         <>
         <div className="right-section">
-        <Link to='/'>Home</Link>
-            <p>wow</p>
-            <p>wow</p>
-            <p>wow</p>
-            <p>wow</p>
-            <p>wow</p>
+            <Link to='/'><button className="nav-button">Home</button></Link>
+            <Link to='/other'><button className="underline nav-button">Info</button></Link>
+            <h1>About this Project</h1>
+            <p>This game is made with React by Charlie Eeles.</p><hr/>
+            <p>Clicking on a monster will deal damage set by the weapon you're using, 
+            defeating monsters will give you gold that you can use to upgrade your
+            weapon and hire party members to fight for you.<br/> The special attack is 
+            an AOE that kills 3 monsters at once but can't be used until you have at least
+            a bow.<br/>Monsters are generated randomly and a boss will show up every 5 monsters.</p><hr/>
+            <h2>Why?</h2>
+            <p>I wanted to make something in React that wasn't just a website so 
+             I decided to have a bit of fun with it.</p><hr/>
+            <h2>Asset Credits</h2>
+            <p>The monsters and weapons are assets from <a href="https://oldschool.runescape.com/">Old School Runescape</a> by <a href="https://www.jagex.com/">Jagex ltd.</a><br/>
+             This project was inspired by <a href="http://orteil.dashnet.org/cookieclicker/">Cookie Clicker</a> by <a href="http://orteil.dashnet.org/">Orteil</a> and <a href="http://dashnet.org/">Opti</a>.
+            </p>
         </div>
         </>
     );
--- src/Shop.js
+++ src/Shop.js
@@ -10,7 +10,8 @@ const Shop = (props) => {
     return(
         <>
         <div className="right-section">
-            <Link to='/other'>other</Link>
+            <Link to='/'><button className="underline nav-button">Home</button></Link>
+            <Link to='/other'><button className="nav-button">Info</button></Link>
             <div className="shop-grid">
                 <Weapons 
                     weaponName={props.weaponName}
